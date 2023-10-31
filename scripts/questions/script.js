4e8d97bc79b0cc0596e18f6bef301098f71fd722
Anion11 <nik.klyukoit@bk.ru>
2023-11-01 02:17:52 +0300

    fix questions-block & add font-bold & add font-family variable & fix class names

--- scripts/questions/script.js
+++ scripts/questions/script.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", function () {
-  let cardBtnExpand = document.getElementsByClassName("questions-expand");
+  let cardBtnExpand = document.getElementsByClassName("questions__expand");
   let cardHeader = document.getElementsByClassName("questions__header");
   let cardText = document.getElementsByClassName("questions__text");
   for (let i = 0; i < cardHeader.length; i++) {
+    cardBtnExpand[i].childNodes[1].classList.toggle("expand-active");
     cardHeader[i].addEventListener("click", function () {
       cardText[i].classList.toggle("is-visible");
-      if (cardBtnExpand[i].style.display == "none")
-        cardBtnExpand[i].style.display = "block";
-      else cardBtnExpand[i].style.display = "none";
+      cardBtnExpand[i].childNodes[1].classList.toggle("expand-active");
+      cardBtnExpand[i].childNodes[3].classList.toggle("expand-active");
     });
   }
 });
