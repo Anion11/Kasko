7bc6067f5d987c58fc8ec7b3f78cb22643a42f3d
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2023-11-02 11:35:06 +0300

    fix questions.js & fix questions button

--- scripts/questions/questions.js
+++ scripts/questions/questions.js
@@ -1,13 +1,9 @@
 window.addEventListener("load", function () {
-  let cardBtnExpand = document.getElementsByClassName("questions__expand");
-  let cardHeader = document.getElementsByClassName("questions__header");
-  let cardText = document.getElementsByClassName("questions__text");
-  for (let i = 0; i < cardHeader.length; i++) {
-    cardBtnExpand[i].childNodes[1].classList.toggle("expand-active");
-    cardHeader[i].addEventListener("click", function () {
-      cardText[i].classList.toggle("is-visible");
-      cardBtnExpand[i].childNodes[1].classList.toggle("expand-active");
-      cardBtnExpand[i].childNodes[3].classList.toggle("expand-active");
-    });
+  let questions__item = document.getElementsByClassName("questions__item");
+  for(let item of questions__item){
+    item.querySelectorAll("div.questions__header")[0].addEventListener("click", (e) =>{
+      item.querySelectorAll("div.questions__header .expand__plus")[1].classList.toggle("expand__plus-active");
+      item.querySelectorAll("div.questions__text")[0].classList.toggle("is-visible");
+    })
   }
 });
