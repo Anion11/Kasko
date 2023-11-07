86eda72203fce543e4422e52f990dfa34d283a78
Anion11 <nik.klyukoit@bk.ru>
2023-11-07 21:05:16 +0300

    init pug

--- scripts/questions/questions.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-  let questions__item = document.getElementsByClassName("questions__item");
-  for(let item of questions__item){
-    item.querySelectorAll("div.questions__header")[0].addEventListener("click", (e) =>{
-      item.querySelectorAll("div.questions__header .expand__plus")[1].classList.toggle("expand__plus-active");
-      item.querySelectorAll("div.questions__text")[0].classList.toggle("is-visible");
-    })
-  }
-});
