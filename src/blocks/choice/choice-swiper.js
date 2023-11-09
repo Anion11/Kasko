3d8379f151a5c15f28474b13632b6763b056fa38
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2023-11-09 11:06:32 +0300

    add choice adaptive mobile & fix layout style

--- src/blocks/choice/choice-swiper.js
+++ src/blocks/choice/choice-swiper.js
@@ -1,20 +1,31 @@
 import Swiper from 'swiper/bundle';
 
 export default function choiceSwiper() {
-  const choiceSwiper = new Swiper('.choice .swiper', {
-    direction: 'horizontal',
-    loop: false,
-    spaceBetween: 20,
-    speed: 500,
-    grabCursor: true,
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      dynamicBullets: true,
-      dynamicMainBullets: 3
-    },
-    keyboard: {
-      enabled: true
-    }
-  });
+  const mediaQuery = window.matchMedia('(max-width: 768px)');
+  let choiceSwiper;
+  function createSwiper() {
+    if (choiceSwiper !== undefined) choiceSwiper.destroy(true, true);
+    choiceSwiper = new Swiper('.choice .swiper', {
+      direction: 'horizontal',
+      loop: false,
+      spaceBetween: 20,
+      speed: 500,
+      grabCursor: true,
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        dynamicMainBullets: 3
+      },
+      keyboard: {
+        enabled: true
+      }
+    });
+  }
+  function breakPointCheck() {
+    if (mediaQuery.matches) createSwiper();
+    else if (choiceSwiper !== undefined) choiceSwiper.destroy(true, true);
+  }
+  mediaQuery.addEventListener('change', breakPointCheck);
+  breakPointCheck();
 }
