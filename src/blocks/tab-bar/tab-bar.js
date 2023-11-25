a167f40980a81feb89e8a4c1b9a44038f7cc1d07
Anion11 <nik.klyukoit@bk.ru>
2023-11-25 12:54:47 +0300

    fix tab-bar

--- src/blocks/tab-bar/tab-bar.js
+++ src/blocks/tab-bar/tab-bar.js
@@ -5,7 +5,9 @@ export default function tabBarMenu() {
   const buttonMenu = document.querySelector('.tab-bar__main');
   const popup = document.querySelector('.menu-popup');
   function toggleMenu() {
-    toggle(popup, { display: 'grid', direction: 'normal' });
+    if (popup.style.display === 'none' || popup.style.display === '') popup.style.display = 'grid';
+    else toggle(popup, { delay: 250, duration: 0 });
+    toggle(popup.firstElementChild, { duration: 300 });
     document.body.style.overflow = document.body.style.overflow === 'hidden' ? 'visible' : 'hidden';
   }
   function closeMenu() {
