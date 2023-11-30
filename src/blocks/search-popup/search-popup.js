8f2e3b0e876313cf8d5c0df3fd43a0dbe3455748
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2023-11-30 18:01:03 +0300

    fix search-popup input

--- src/blocks/search-popup/search-popup.js
+++ src/blocks/search-popup/search-popup.js
@@ -3,13 +3,19 @@ import { down, up } from 'slide-element'
 export default function SearchPopup() {
   const popup = document.querySelector('.search-popup')
   const popupButtons = document.querySelectorAll('.search__popup-open')
+  const input = popup.querySelector('input')
+  let count = 0
   function openPopup() {
+    count++
+    input.value = ''
+    input.dispatchEvent(new Event('input'))
     down(popup, { display: 'grid', duration: 0 })
     document.body.style.overflow = 'hidden'
   }
   function closePopup() {
     up(popup, { duration: 0 })
-    document.body.style.overflow = 'visible'
+    count--
+    if (count === 0) document.body.style.overflow = 'visible'
   }
   for (const popupButton of popupButtons) {
     popupButton.addEventListener('click', () => {
