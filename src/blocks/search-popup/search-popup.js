badd891f607458a9f1488c3ad0831a024a823161
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2023-11-30 11:14:55 +0300

    fix popup-search

--- src/blocks/search-popup/search-popup.js
+++ src/blocks/search-popup/search-popup.js
@@ -2,26 +2,23 @@ import { down, up } from 'slide-element'
 
 export default function SearchPopup() {
   const popup = document.querySelector('.search-popup')
-  const buttonSearch = document.querySelectorAll('.searchButton')
-  const popupCloseCross = document.querySelector('.search-popup__close-cross')
-  const inputSearch = document.querySelector('#search-popup')
+  const popupButtons = document.querySelectorAll('.search__popup-open')
+  function openPopup() {
+    down(popup, { display: 'grid', duration: 0 })
+    document.body.style.overflow = 'hidden'
+  }
   function closePopup() {
     up(popup, { duration: 0 })
-    inputSearch.value = ''
-    inputSearch.dispatchEvent(new Event('input'))
     document.body.style.overflow = 'visible'
   }
-  function openPopup() {
-    down(popup, { display: 'flex', duration: 0 })
-    document.body.style.overflow = 'hidden'
+  for (const popupButton of popupButtons) {
+    popupButton.addEventListener('click', () => {
+      openPopup()
+    })
   }
   popup.addEventListener('click', (event) => {
     if (event.target === popup) closePopup()
   })
+  const popupCloseCross = popup.querySelector('.search-popup__close')
   popupCloseCross.addEventListener('click', closePopup)
-  if (buttonSearch) {
-    for (const buttonSearchItem of buttonSearch) {
-      buttonSearchItem.addEventListener('click', openPopup)
-    }
-  }
 }
