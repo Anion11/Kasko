9852b54158f2154a7242aa6af08d53da9e065120
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2023-12-04 17:54:33 +0300

    fix linter in search-popup.js & fix search-popup style

--- src/blocks/search-popup/search-popup.js
+++ src/blocks/search-popup/search-popup.js
@@ -6,19 +6,17 @@ export default function SearchPopup() {
   const input = popup.querySelector('input')
   let flag = false
   function checkOpenPopup() {
-    return document.body.classList.value === "popup-active"
+    return document.body.classList.value === 'popup-active'
   }
   function openPopup() {
     input.value = ''
     input.dispatchEvent(new Event('input'))
     down(popup, { display: 'grid', duration: 0 })
-    console.log(checkOpenPopup())
-    checkOpenPopup() ? flag = true : document.body.classList.add("popup-active") 
-    console.log(flag)
+    checkOpenPopup() ? flag = true : document.body.classList.add('popup-active')
   }
   function closePopup() {
     up(popup, { duration: 0 })
-    !flag && document.body.classList.remove("popup-active")
+    !flag && document.body.classList.remove('popup-active')
   }
   for (const popupButton of popupButtons) {
     popupButton.addEventListener('click', () => {
