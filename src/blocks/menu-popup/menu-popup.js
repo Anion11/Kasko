f9b213e20c415d14fc9695e2ba3fd5f024649280
Anion11 <nik.klyukoit@bk.ru>
2023-12-05 20:16:41 +0300

    fix touch bar

--- src/blocks/menu-popup/menu-popup.js
+++ src/blocks/menu-popup/menu-popup.js
@@ -1,4 +1,4 @@
-import { toggle, up, down } from 'slide-element'
+import { toggle, up } from 'slide-element'
 
 export default function menuPopup() {
   const breakPoint = window.matchMedia('(max-width: 1440px)')
@@ -6,9 +6,9 @@ export default function menuPopup() {
   const popup = document.querySelector('.menu-popup')
   const popupCloseCross = document.querySelector('.menu-popup__close-cross')
   const popupCloseDrag = document.querySelector('.menu-popup__close-drag')
-  var touchStart = null;
-  var touchPosition = null;
-  let currentPosition = null
+  let touchStart
+  let touchPosition
+  let currentPosition
   function toggleMenu() {
     if (popup.style.display === 'none' || popup.style.display === '') popup.style.display = 'grid'
     else toggle(popup, { delay: 250, duration: 0 })
@@ -20,35 +20,42 @@ export default function menuPopup() {
     up(popup.firstElementChild, { duration: 300 })
     document.body.classList.remove('popup-active')
   }
-  function TouchStart(e) {
-    touchStart = e.changedTouches[0].clientY;
+  function TouchStart(event) {
+    popup.firstElementChild.style.transition = 'transform 0s'
+    touchStart = event.changedTouches[0].clientY
   }
-  function TouchMove(e) {
-    touchPosition = e.changedTouches[0].clientY;
+  function TouchMove(event) {
+    touchPosition = event.changedTouches[0].clientY
     currentPosition = touchPosition - touchStart
     if (currentPosition > 0) popup.firstElementChild.style.transform = 'translateY(' + currentPosition + 'px)'
   }
   function TouchEnd() {
-    if (currentPosition < 300) {
+    if (currentPosition < 200) {
+      popup.firstElementChild.style.transition = 'transform .3s'
       popup.firstElementChild.style.transform = 'translateY(' + 0 + 'px)'
-      currentPosition = null
-      touchStart = null;
-      touchPosition = null;
-    }
-    else {
+      currentPosition = undefined
+      touchStart = undefined
+      touchPosition = undefined
+    } else {
       closeMenu()
-      setTimeout(()=>{
-        popup.firstElementChild.style.transform = 'translateY(' + 0 + 'px)' 
-      }, 1000)
+      setTimeout(() => {
+        popup.firstElementChild.style.transform = 'translateY(' + 0 + 'px)'
+      }, 300)
     }
   }
   popup.addEventListener('click', (event) => {
     if (event.target === popup) closeMenu()
   })
   popupCloseCross.addEventListener('click', closeMenu)
-  popupCloseDrag.addEventListener("touchstart", function (e) { TouchStart(e) });
-  popupCloseDrag.addEventListener("touchmove", function (e) { TouchMove(e) });
-  popupCloseDrag.addEventListener("touchend", function (e) { TouchEnd() });
+  popupCloseDrag.addEventListener('touchstart', (event) => {
+    TouchStart(event)
+  })
+  popupCloseDrag.addEventListener('touchmove', (event) => {
+    TouchMove(event)
+  })
+  popupCloseDrag.addEventListener('touchend', () => {
+    TouchEnd()
+  })
   buttonMenu.addEventListener('click', toggleMenu)
   breakPoint.addEventListener('change', closeMenu)
 }
