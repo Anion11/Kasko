c24a055b6ba128f5dbd3d88772dcd4855328b6c1
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2023-12-05 17:54:01 +0300

    add touchbar js in menu-popup

--- src/blocks/menu-popup/menu-popup.js
+++ src/blocks/menu-popup/menu-popup.js
@@ -1,10 +1,14 @@
-import { toggle, up } from 'slide-element'
+import { toggle, up, down } from 'slide-element'
 
 export default function menuPopup() {
   const breakPoint = window.matchMedia('(max-width: 1440px)')
   const buttonMenu = document.querySelector('.tab-bar__main')
   const popup = document.querySelector('.menu-popup')
   const popupCloseCross = document.querySelector('.menu-popup__close-cross')
+  const popupCloseDrag = document.querySelector('.menu-popup__close-drag')
+  var touchStart = null;
+  var touchPosition = null;
+  let currentPosition = null
   function toggleMenu() {
     if (popup.style.display === 'none' || popup.style.display === '') popup.style.display = 'grid'
     else toggle(popup, { delay: 250, duration: 0 })
@@ -16,10 +20,35 @@ export default function menuPopup() {
     up(popup.firstElementChild, { duration: 300 })
     document.body.classList.remove('popup-active')
   }
+  function TouchStart(e) {
+    touchStart = e.changedTouches[0].clientY;
+  }
+  function TouchMove(e) {
+    touchPosition = e.changedTouches[0].clientY;
+    currentPosition = touchPosition - touchStart
+    if (currentPosition > 0) popup.firstElementChild.style.transform = 'translateY(' + currentPosition + 'px)'
+  }
+  function TouchEnd() {
+    if (currentPosition < 300) {
+      popup.firstElementChild.style.transform = 'translateY(' + 0 + 'px)'
+      currentPosition = null
+      touchStart = null;
+      touchPosition = null;
+    }
+    else {
+      closeMenu()
+      setTimeout(()=>{
+        popup.firstElementChild.style.transform = 'translateY(' + 0 + 'px)' 
+      }, 1000)
+    }
+  }
   popup.addEventListener('click', (event) => {
     if (event.target === popup) closeMenu()
   })
   popupCloseCross.addEventListener('click', closeMenu)
+  popupCloseDrag.addEventListener("touchstart", function (e) { TouchStart(e) });
+  popupCloseDrag.addEventListener("touchmove", function (e) { TouchMove(e) });
+  popupCloseDrag.addEventListener("touchend", function (e) { TouchEnd() });
   buttonMenu.addEventListener('click', toggleMenu)
   breakPoint.addEventListener('change', closeMenu)
 }
